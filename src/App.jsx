ba956140af605901acd46c845c97dad2775c54a3
Cole Rossman <colerossman99@gmail.com>
2022-05-02 16:10:30 -0700

    App.jsx uses browserrouter, switch and route to establish url paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header'
+import Main from './views/Main/Main'
+import CharacterDetail from './views/CharacterDetail/CharacterDetail';
+import { BrowserRouter } from 'react-router-dom';
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route path="/character/:id">
+        <CharacterDetail />
+      </Route>
+      <Route path="/">
+        <Main />
+      </Route>
+    </Switch>
+    </BrowserRouter>
+    
+  )
 }
