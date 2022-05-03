abffd336d3511430fac2d231e310a754d7766866
Cole Rossman <colerossman99@gmail.com>
2022-05-02 18:03:19 -0700

    characters in services deleted because no longer needed

--- src/services/characters.js
+++
@@ -1,5 +0,0 @@
-export async function fetchCharacters() {
-    const resp = await fetch('https://rickandmortyapi.com/api/character?count=20');
-    const data = await resp.json();
-    return data;
-}
